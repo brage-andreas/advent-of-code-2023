ce56cb7867b81943fa75525f61429d928dd89be4
brage-andreas <draaangooo@gmail.com>
2023-12-01 23:20:18 +0100

    feat: use function from `part_1.rs` and remove tests

--- day-01/src/bin/part_2.rs
+++ day-01/src/bin/part_2.rs
@@ -24,6 +24,8 @@
     What is the sum of all of the calibration values?
 */
 
+mod part_1;
+
 use regex::Regex;
 use std::fs;
 
@@ -60,13 +62,6 @@ fn get_first_and_last_number(string: &str) -> (&str, &str) {
     (first_number, second_number)
 }
 
-fn combine_first_and_last_number(first_number: &str, second_number: &str) -> i32 {
-    match format!("{}{}", first_number, second_number).parse::<i32>() {
-        Ok(result) => result,
-        Err(_) => panic!("Regex `REGEX` is faulty, as it captured a non-number"),
-    }
-}
-
 fn part_2() -> i32 {
     let file = match fs::read_to_string(&PART_2_INPUT_FILE_PATH) {
         Ok(result) => result,
@@ -81,7 +76,7 @@ fn part_2() -> i32 {
     for line in file.split("\n") {
         let (first_number, last_number) = get_first_and_last_number(line);
 
-        result += combine_first_and_last_number(&first_number, &last_number);
+        result += part_1::combine_first_and_last_number(&first_number, &last_number);
     }
 
     result
@@ -107,13 +102,8 @@ mod tests {
         assert_eq!(match_capture("eight"), "8");
         assert_eq!(match_capture("nine"), "9");
         assert_eq!(match_capture("1"), "1");
-        assert_eq!(match_capture("2"), "2");
-        assert_eq!(match_capture("3"), "3");
         assert_eq!(match_capture("4"), "4");
         assert_eq!(match_capture("5"), "5");
-        assert_eq!(match_capture("6"), "6");
-        assert_eq!(match_capture("7"), "7");
-        assert_eq!(match_capture("8"), "8");
         assert_eq!(match_capture("9"), "9");
     }
 
@@ -122,19 +112,8 @@ mod tests {
         assert_eq!(get_first_and_last_number("two1nine"), ("2", "9"));
         assert_eq!(get_first_and_last_number("eightwothree"), ("8", "3"));
         assert_eq!(get_first_and_last_number("abcone2threexyz"), ("1", "3"));
-        assert_eq!(get_first_and_last_number("xtwone3four"), ("2", "4"));
-        assert_eq!(get_first_and_last_number("4nineeightseven2"), ("4", "2"));
-        assert_eq!(get_first_and_last_number("zoneight234"), ("1", "4"));
         assert_eq!(get_first_and_last_number("7pqrstsixteen"), ("7", "6"));
         assert_eq!(get_first_and_last_number("6zfxp"), ("6", "6"));
-    }
-
-    #[test]
-    fn combine_first_and_last_number_test() {
-        assert_eq!(combine_first_and_last_number("1", "2"), 12);
-        assert_eq!(combine_first_and_last_number("3", "8"), 38);
-        assert_eq!(combine_first_and_last_number("1", "5"), 15);
-        assert_eq!(combine_first_and_last_number("7", "7"), 77);
     }
 
     #[test]
