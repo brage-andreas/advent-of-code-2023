f887aac734524dffdd8370c376bcb94642fd633b
brage-andreas <draaangooo@gmail.com>
2023-12-03 17:54:21 +0100

    feat: add `read_input`

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,5 +1,9 @@
 use std::fs;
 
+fn generate_path_from_day(day: u8) -> String {
+    format!("day-{:02}/src/day-{:02}-input.txt", day, day)
+}
+
 pub fn read_from_file(path_from_workspace_root: &str) -> String {
     match fs::read_to_string(&path_from_workspace_root) {
         Ok(result) => result,
@@ -8,4 +12,10 @@ pub fn read_from_file(path_from_workspace_root: &str) -> String {
             &path_from_workspace_root, &error
         ),
     }
+}
+
+pub fn read_input(day: u8) -> String {
+    let path_from_workspace_root = generate_path_from_day(day);
+
+    read_from_file(&path_from_workspace_root)
 }
